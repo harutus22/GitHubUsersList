6f83b6ff2bfcbd179bd43c6f634a00da445220dd
harutus22 <harutus22@yahoo.com>
2019-02-07 17:16:58 +0400

    Repos list View

---
+++ app/src/main/java/com/example/apple/githubuserslist/activity/activity/user/Repo.java
@@ -0,0 +1,18 @@
+package com.example.apple.githubuserslist.activity.activity.user;
+
+public class Repo {
+
+    private String name;
+
+    public Repo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
